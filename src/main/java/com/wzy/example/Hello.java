25e6e28adeb455dc4e0ba1e06e616a889566cd07
wzy <wzy@163.com>
2021-04-28 15:07:22 +0800

    git-demo second commit

--- src/main/java/com/wzy/example/Hello.java
+++ src/main/java/com/wzy/example/Hello.java
@@ -3,5 +3,6 @@ package com.wzy.example;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git second");
     }
 }
