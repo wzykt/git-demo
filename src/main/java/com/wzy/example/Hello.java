c547b9951ef35ce0d2c3d6219b94399c203c47b1
wzy <wzy@163.com>
2021-04-28 16:17:00 +0800

    hot-fix recommit

--- src/main/java/com/wzy/example/Hello.java
+++ src/main/java/com/wzy/example/Hello.java
@@ -3,9 +3,5 @@ package com.wzy.example;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
-        System.out.println("Hello git second");
-        System.out.println("hello git hot-fix");
-        System.out.println("hello git hot-fix second");
-        System.out.println("hello git hot-fix third");
     }
 }
