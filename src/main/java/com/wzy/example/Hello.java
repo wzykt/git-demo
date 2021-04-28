5a65cf966fdb706119aad22148bccd37b5959f63
wzy <wzy@163.com>
2021-04-28 16:11:20 +0800

    git-demo hot-fix fourth commit

--- src/main/java/com/wzy/example/Hello.java
+++ src/main/java/com/wzy/example/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
         System.out.println("Hello git");
         System.out.println("Hello git second");
         System.out.println("Hello git third");
+        System.out.println("Hello git fourth");
         System.out.println("hello git hot-fix");
         System.out.println("hello git hot-fix second");
     }
