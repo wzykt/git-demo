6f14b99f332341c5492f2ef7cb7893cbfc9c5278
wzy <wzy@163.com>
2021-04-28 16:08:07 +0800

    git-demo third commit

--- src/main/java/com/wzy/example/Hello.java
+++ src/main/java/com/wzy/example/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git second");
+        System.out.println("Hello git third");
         System.out.println("hello git hot-fix");
     }
 }
