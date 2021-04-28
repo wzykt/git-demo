e8f5de669c7527450c38973435371f121a798e68
wzy <wzy@163.com>
2021-04-28 15:59:23 +0800

    git-demo hot-fix first commit

--- src/main/java/com/wzy/example/Hello.java
+++ src/main/java/com/wzy/example/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git second");
+        System.out.println("hello git hot-fix");
     }
 }
